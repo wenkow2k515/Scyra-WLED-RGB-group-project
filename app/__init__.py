9a46feff4e0be3b1d93e91525c985d5d32f0ffe5
wenkow <1220465967@qq.com>
2025-04-16 22:28:39 -0400

    init the three route with simple infor

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,17 @@ import app.routes
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
+@app.route('/')
+def home():
+    return render_template('home.html', title='Home')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
 
 
 if __name__ == '__main__':
