5701f83094cb749aca49375131fb6b7daeaf07cd
wenkow <1220465967@qq.com>
2025-04-17 00:50:38 -0400

    init the three route with simple infor

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,21 @@ from flask import Flask, url_for, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, BooleanField
 from wtforms.validators import DataRequired, Length, NumberRange
-import app.routes
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
+@app.route('/')
+def home():
+    return render_template('home.html', title='Home')
 
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
 
 
 if __name__ == '__main__':
