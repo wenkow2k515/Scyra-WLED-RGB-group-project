c9b4e61d12e4ac7b47ded3bb67e4f3be2900c176
wenkow <1220465967@qq.com>
2025-04-30 09:27:12 -0400

    the file that contain the infor for the database and where they store

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin  # Add this import
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):  # Add UserMixin inheritance
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
@@ -17,7 +18,6 @@ class UploadedData(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # reference to User
     preset_name = db.Column(db.String(100), nullable=False)  # preset name
     preset_data = db.Column(db.JSON, nullable=False)  # preset data stored as JSON
-    user = db.relationship('User', backref=db.backref('uploads', lazy=True))  # relationship to User
 
 class SharedData(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # shared data id
@@ -25,5 +25,3 @@ class SharedData(db.Model):
     shared_with_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # user who received
     preset_id = db.Column(db.Integer, db.ForeignKey('uploaded_data.id'), nullable=False)  # shared preset
     shared_by = db.relationship('User', foreign_keys=[shared_by_id], backref=db.backref('shared_by', lazy=True))  # relationship for sharer
-    shared_with = db.relationship('User', foreign_keys=[shared_with_id], backref=db.backref('shared_with', lazy=True))  # relationship for receiver
-    preset = db.relationship('UploadedData', backref=db.backref('shared_presets', lazy=True))  # relationship to UploadedData
